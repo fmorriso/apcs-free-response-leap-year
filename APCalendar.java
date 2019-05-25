a13f7429a7dbf75b0ea9520ce372c4dd76cc8235
Fred Morrison <fpmorrison@verizon.net>
2019-05-25 17:56:59 -0400

    Update APCalendar.java

--- APCalendar.java
+++ APCalendar.java
@@ -50,7 +50,8 @@ public class APCalendar
 	}
 
 	/**
-	 * returns the value representing the day of the week for the first day of year, where 0 denotes Sunday, 1 denotes Monday, ..., and 6 denotes Saturday.
+	 * returns the value representing the day of the week for the first day of year, 
+	 * where 0 denotes Sunday, 1 denotes Monday, ..., and 6 denotes Saturday.
 	 * 
 	 * Precondition: The date represented by month, day, year is a valid date.
 	 * 
